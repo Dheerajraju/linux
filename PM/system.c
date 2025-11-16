41114c1acd66e6036b0c513b039a717fb4231abe
Dheeraj raju <dheshal07@gmail.com>
2025-11-16 21:29:34 +0530

    Create system.c

---
+++ PM/system.c
@@ -0,0 +1,25 @@
+//main.c
+#include<stdoi.h>
+#include<stdlib.h>
+int main()
+{
+  printf("hi..\n");
+system("./p1");
+system("pwd");
+printf("hello..\n");
+}
+
+
+
+
+//p1.c
+#include<stdio.h>
+int main()
+{
+  printf("p1 in process..\n");
+while(1);
+}
+
+
+
+// $ps -el | grep pts/0
